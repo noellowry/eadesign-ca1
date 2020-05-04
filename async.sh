e9acfff49749ff509824f7c6685ce01041c51397
Noel Lowry <X00159364@mytudublin.ie>
2020-05-04 16:24:40 +0100

    Update

--- async.sh
+++ async.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+total_time=0;
+count=200;
 echo "ASYNC Response Time";
-for((i=1;i<=10;i++)); 
+for((i=1;i<=$count;i++)); 
 do 
-  curl http://35.228.112.214:31080/ -s -o /dev/null -w "Output: starttransfer:%{time_starttransfer}s total:%{time_total}\n" ; 
+  curl_result="$(curl http://35.228.112.214:31080/ -s -o /dev/null -w "Output: starttransfer:%{time_starttransfer}s total:%{time_total}\n")"
+  echo $curl_result 
+
+  var=$(echo $curl_result | awk -F":" '{print $1, $2, $3, $4 }')
+  set -- $var
+  echo "$1 $2 $3 $4 $5"
+  total_time=`echo "scale=6; $total_time + $5" | bc`;
 done
+#echo "scale=6;total_time: $total_time"
+echo "average time taken: `echo "scale=6; $total_time/$count" | bc`";
