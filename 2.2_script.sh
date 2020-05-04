972f632f6bd88e9478b3754a314304988aa0b12f
Noel Lowry <noel.lowry@sap.com>
2020-05-04 21:08:54 +0100

    Update

--- 2.2_script.sh
+++ 2.2_script.sh
@@ -5,7 +5,7 @@ count=5;
 avg_sync_rsp=0;
 avg_async_rsp=0;
 #################################
-#  Part 2.1                     #
+#  Part 2.2                     #
 #################################
 # get_response_time(){
 #   for((i=1;i<=$1;i++)); 
@@ -23,22 +23,6 @@ avg_async_rsp=0;
 # }
 # get_response_time $count, "http://35.228.112.214:30916"
 
-#Get SYNC RESPONSE TIME
-for((i=1;i<=$count;i++)); 
-do 
-  curl_result="$(curl http://35.228.112.214:30916/ -s -o /dev/null -w "total:%{time_total}\n")"
-  #echo $curl_result 
-
-  var=$(echo $curl_result | awk -F":" '{print $1, $2}')
-  set -- $var
-  echo "$1 $2"
-  total_time=$(bc <<< "scale=6; $total_time + $2")
-done
-
-avg_sync_rsp=$(bc <<< "scale=6; $total_time/$count")
-#echo $avg_sync_rsp;
-#echo "SYNC Average Response Time: `echo "scale=6; $avg_sync_rsp" | bc`";
-
 #Get ASYNC RESPONSE TIME
 for((i=1;i<=$count;i++)); 
 do 
@@ -52,11 +36,7 @@ do
 done
 avg_async_rsp=$(bc <<< "scale=6; $total_time/$count")
 
-echo "SYNC Average Response Time: `echo "scale=6; $avg_sync_rsp" | bc`";
 echo "ASYNC Average Response Time: `echo "scale=6; $avg_async_rsp" | bc`";
 
-gcloud functions call create-graph --project eadesign-269520 --data '{"filename":"2.1-graph-'"$DATE"'.png", "plottype":"bar", "x":["SYNC", "ASYNC"], "y":["'"$avg_sync_rsp"'", "'"$avg_async_rsp"'"], "ylab":"Time in Seconds"}';
-
-#################################
-#  Part 2.2                     #
-#################################
+gcloud functions call create-graph --project eadesign-269520 --data '{"filename":"2.2-graph-'"$DATE"'.png", "plottype":"line", "x":["2000", "10000", "20000"], "y":["0.066996","00.067789","0.067247", "0.066995", "
+0.067226", "0.066946", "0.066934", "0.066439", "0.066958"], "ylab":["Door publish interval: 10ms", "Door publish interval: 50ms", "Door publish interval: 100ms"]}';
---
+++ 2.3_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DATE=$(date +"%Y%m%d%H%M%S")
+total_time=0;
+count=5;
+avg_sync_rsp=0;
+avg_async_rsp=0;
+#################################
+#  Part 2.3                     #
+#################################
+kubectl delete pod -l app=seccon-sync -n ca-dev 
+kubectl delete pod -l app=door1-sync -n ca-dev 
+kubectl delete pod -l app=door2-sync -n ca-dev 
+
+kubectl delete pod -l app=seccon -n ca-dev 
+kubectl delete pod -l app=door1 -n ca-dev 
+kubectl delete pod -l app=door2 -n ca-dev 
+
+kubectl get all -n ca-dev
+
+#gcloud functions call create-graph --project eadesign-269520 --data '{"filename":"2.3-graph-'"$DATE"'.png", "plottype":"line", "x":["2000", "10000", "20000"], "y":["0.066996","00.067789","0.067247", "0.066995", "0.067226", "0.066946", "0.066934", "0.066439", "0.066958"], "ylab":["Door publish interval: 10ms", "Door publish interval: 50ms", "Door publish interval: 100ms"]}';
