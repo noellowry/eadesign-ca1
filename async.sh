b485e8b5f64e71e67dec4ad3864072bfa5c2cfc2
Noel Lowry <noel.lowry@sap.com>
2020-05-04 20:05:39 +0100

    Update

--- async.sh
+++ async.sh
@@ -7,25 +7,23 @@ avg_async_rsp=0;
 #################################
 #  Part 2.1                     #
 #################################
-# #Get SYNC RESPONSE TIME
-# echo "SYNC Response Time";
-# for((i=1;i<=$count;i++)); 
-# do 
-#   curl_result="$(curl http://35.228.112.214:30916/ -s -o /dev/null -w "Output: starttransfer:%{time_starttransfer}s total:%{time_total}\n")"
-#   #echo $curl_result 
+#Get SYNC RESPONSE TIME
+for((i=1;i<=$count;i++)); 
+do 
+  curl_result="$(curl http://35.228.112.214:30916/ -s -o /dev/null -w "total:%{time_total}\n")"
+  #echo $curl_result 
 
-#   var=$(echo $curl_result | awk -F":" '{print $1, $2, $3, $4 }')
-#   set -- $var
-#   echo "$1 $2 $3 $4 $5"
-#   total_time=`echo "scale=6; $total_time + $5" | bc`;
-# done
-# #echo "scale=6;total_time: $total_time"
-# avg_sync_rsp=$total_time/$count;
-# echo $avg_async_rsp;
-# echo "SYNC average time taken: `echo "scale=6; $total_time/$count" | bc`";
+  var=$(echo $curl_result | awk -F":" '{print $1, $2}')
+  set -- $var
+  echo "$1 $2"
+  total_time=$(bc <<< "scale=6; $total_time + $2")
+done
+
+avg_sync_rsp=$(bc <<< "scale=6; $total_time/$count")
+#echo $avg_sync_rsp;
+#echo "SYNC Average Response Time: `echo "scale=6; $avg_sync_rsp" | bc`";
 
 #Get ASYNC RESPONSE TIME
-echo "ASYNC Response Time";
 for((i=1;i<=$count;i++)); 
 do 
   curl_result="$(curl http://35.228.112.214:31080/ -s -o /dev/null -w "total:%{time_total}\n")"
@@ -34,13 +32,14 @@ do
   var=$(echo $curl_result | awk -F":" '{print $1, $2}')
   set -- $var
   echo "$1 $2"
-  total_time=`echo "scale=6; $total_time + $2" | bc`;
+  total_time=$(bc <<< "scale=6; $total_time + $2")
 done
-#echo "scale=6;total_time: $total_time"
-echo "ASYNC average time taken: `echo "scale=6; $total_time/$count" | bc`";
-#echo $DATE
+avg_async_rsp=$(bc <<< "scale=6; $total_time/$count")
 
-gcloud functions call create-graph --project eadesign-269520 --data '{"filename":"2.1-graph-'"$DATE"'.png", "plottype":"line", "x":["2000", "10000", "20000"], "y":["0.066996","00.067789","0.067247", "0.066995", "0.067226", "0.066946", "0.066934", "0.066439", "0.066958"], "ylab":["Door publish interval: 10ms", "Door publish interval: 50ms", "Door publish interval: 100ms"]}';
+echo "SYNC Average Response Time: `echo "scale=6; $avg_sync_rsp" | bc`";
+echo "ASYNC Average Response Time: `echo "scale=6; $avg_async_rsp" | bc`";
+
+gcloud functions call create-graph --project eadesign-269520 --data '{"filename":"2.1-graph-'"$DATE"'.png", "plottype":"bar", "x":["SYNC", "ASYNC"], "y":["'"$avg_sync_rsp"'", "'"$avg_async_rsp"'"], "ylab":"Time in Seconds"}';
 
 #################################
 #  Part 2.2                     #
